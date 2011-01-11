c4bea1adcd40aa462adbb0e4e948877911250bc6
Rafael George <george.rafael@gmail.com>
2011-01-11 18:52:50 +0000

    initial commit

---
+++ lib/provider/ticket.rb
@@ -0,0 +1,11 @@
+module TicketMaster::Provider
+  module Bugzilla
+    # Ticket class for ticketmaster-bugzilla
+    #
+    
+    class Ticket < TicketMaster::Provider::Base::Ticket
+      # declare needed overloaded methods here
+      
+    end
+  end
+end
---
+++ lib/ticketmaster-bugzilla.rb
@@ -0,0 +1,4 @@
+%w{ bugzilla ticket project comment }.each do |f|
+  require File.dirname(__FILE__) + '/provider/' + f + '.rb';
+end
+

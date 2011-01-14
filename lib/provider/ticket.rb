89359b10c2ef5d3d5a7d1f7ec1d5750bd0444cc3
Rafael George <george.rafael@gmail.com>
2011-01-14 13:03:47 +0000

    initial code for tickets behaviour - add stubbing for the list method of rubyzilla - add the ability to load all tickets - add ability to search tickets with an array of id's - add a pending spec to search by attributes

--- lib/provider/ticket.rb
+++ lib/provider/ticket.rb
@@ -5,7 +5,43 @@ module TicketMaster::Provider
     
     class Ticket < TicketMaster::Provider::Base::Ticket
       # declare needed overloaded methods here
-      
+      TICKETS_API = Rubyzilla::Product
+      def initialize(*object)
+        object = object.first
+        @system_data = {:client => object}
+        unless object.is_a? Hash
+          hash = {:product_id => object.product_id,
+                  :id => object.id,
+                  :component_id => object.component_id,
+                  :summary => object.summary,
+                  :version => object.version,
+                  :op_sys => object.op_sys,
+                  :platform => object.platform,
+                  :priority => object.priority,
+                  :description => object.description,
+                  :alias => object.alias,
+                  :qa_contact => object.qa_contact,
+                  :status => object.status,
+                  :target_milestone => object.target_milestone,
+                  :severity => object.severity}
+        else
+          hash = object
+        end
+        super hash
+      end
+
+      def self.find_by_id(id)
+        self.new Rubyzilla::Bug.new id
+      end
+
+      def self.find(project_id, *options)
+        if options.first.empty?
+          TICKETS_API.new(project_id).bugs.collect { |bug| self.new bug }
+        elsif options.first.is_a? Array
+          options[0].first.collect { |id| self.find_by_id(id) }
+        end
+      end
+
     end
   end
 end

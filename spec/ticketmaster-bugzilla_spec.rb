5ea1e41cbefc0433e009df887da7d8196f4c38ee
Rafael George <george.rafael@gmail.com>
2011-06-02 20:31:13 +0000

    added pending spec to test for valid? method for authentication

--- spec/ticketmaster-bugzilla_spec.rb
+++ spec/ticketmaster-bugzilla_spec.rb
@@ -2,10 +2,18 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Ticketmaster::Provider::Bugzilla" do
   
-  it "should be able to instantiate a new instance" do
+  before(:each) do 
     @ticketmaster = TicketMaster.new(:bugzilla, {:username => 'george.rafael@gmail.com', :password => '123456', :url => 'https://landfill.bugzilla.org/bugzilla-3.6-branch'})
+  end
+
+  it "should be able to instantiate a new instance" do
     @ticketmaster.should be_an_instance_of(TicketMaster)
     @ticketmaster.should be_a_kind_of(TicketMaster::Provider::Bugzilla)
   end
-  
+
+  it "should be able to validate it's authentication" do 
+    pending
+    @ticketmaster.valid?.should be_true
+  end
+
 end

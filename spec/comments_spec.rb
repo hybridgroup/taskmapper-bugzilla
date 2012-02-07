300cb129f736411be8da4e6321a9de469b8e0b01
Rafael George <george.rafael@gmail.com>
2012-02-06 21:10:36 -0400

    fixed comments specs

--- spec/comments_spec.rb
+++ spec/comments_spec.rb
@@ -7,26 +7,26 @@ describe "Ticketmaster::Provider::Bugzilla::Comment" do
   end
 
   before(:each) do 
-    @ticketmaster = TicketMaster.new(:bugzilla, {:username => 'george.rafael@gmail.com', :password => '123456', :url => 'https://landfill.bugzilla.org/bugzilla-3.6-branch'})
-    @project = @ticketmaster.project(1)
-    @ticket = @project.ticket(7039)
+    VCR.use_cassette('init-tm-for-comments') { @ticketmaster = TicketMaster.new(:bugzilla, {:username => 'george.rafael@gmail.com', :password => '123456', :url => 'https://landfill.bugzilla.org/bugzilla-3.6-branch'}) }
+    VCR.use_cassette('project-for-comment') { @project = @ticketmaster.project(20) }
+    VCR.use_cassette('ticket-for-comment') { @ticket = @project.ticket(7039) }
   end
 
   it "should be able to load all comments from a ticket" do
-    comments = @ticket.comments
-    comments.should be_an_instance_of(Array)
-    comments.first.should be_an_instance_of(@klass)
+    VCR.use_cassette('comments-all') { @comments = @ticket.comments }
+    @comments.should be_an_instance_of(Array)
+    @comments.first.should be_an_instance_of(@klass)
   end
 
   it "should be able to load all comments based on array of id's" do 
-    comments = @ticket.comments([18575])
-    comments.should be_an_instance_of(Array)
-    comments.first.should be_an_instance_of(@klass)
+    VCR.use_cassette('comments-by-array') { @comments = @ticket.comments([18575]) }
+    @comments.should be_an_instance_of(Array)
+    @comments.first.should be_an_instance_of(@klass)
   end
 
   it "should be able to load a comment based on an id" do 
-    comment = @ticket.comment(18575)
-    comment.should be_an_instance_of(@klass)
-    comment.id.should == 18575
+    VCR.use_cassette('comments-by-id') { @comment = @ticket.comment(18575) }
+    @comment.should be_an_instance_of(@klass)
+    @comment.id.should == 18575
   end
 end
